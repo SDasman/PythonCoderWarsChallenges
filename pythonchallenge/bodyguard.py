ca9851e0b3f38a7e9f57f47a7cc880da619ee1e9
SDasman <shijit.dasgupta@gmail.com>
2021-02-11 22:06:07 -0500

    Update bodyguard.py
    I pythonized this. Mainly by checking a character before and after we can properly do this.

--- pythonchallenge/bodyguard.py
+++ pythonchallenge/bodyguard.py
@@ -1,19 +1,5 @@
-from collections import Counter
 filename = "body.txt"
 import re
 with open(filename) as f:
-    text = f.read()
-i = 0
-msg = ""
-while i < len(text)-7:
-    phrase = text[i:i+7]
-    x = re.search(r'^[A-Z]{3}[a-z][A-Z]{3}', phrase)
-    if x:
-        print(phrase)
-        msg += phrase[3]
-        i += 7
-        continue
-    else:
-        continue
-    i += 1
-print(msg)
+    matches = re.findall(r'[a-z][A-Z]{3}[a-z][A-Z]{3}[a-z]', f.read())
+print("".join([match[4] for match in matches]))
